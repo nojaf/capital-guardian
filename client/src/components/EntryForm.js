23c0819e7a9a1b962a9734277efddff2b7a64641
nojaf <florian.verdonck@outlook.com>
2019-12-24 15:44:22 +0100

    Updated import of useForm

--- client/src/components/EntryForm.js
+++ client/src/components/EntryForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, FormGroup, Input, Button } from "reactstrap";
-import useForm from "react-hook-form";
+import { useForm } from "react-hook-form";
 import PropTypes from "prop-types";
 import * as yup from "yup";
 import ToggleButton from "./ToggleButton";
